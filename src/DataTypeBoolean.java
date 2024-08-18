897bf80c79d2cae31fcd03e118f8c2f853958b71
dkaitha <dineshk.oradb@gmail.com>
2024-08-18 19:55:32 -0400

    uncommenting the code

--- src/DataTypeBoolean.java
+++ src/DataTypeBoolean.java
@@ -1,9 +1,9 @@
-public class DataTypes {
-//    public static void main (String args[]){
-//        boolean A= true;
-//        if(A ==true)
-//            System.out.println("print true");
-//    }
+public class DataTypeBoolean {
+    public static void main (String args[]){
+        boolean A= true;
+        if(A ==true)
+            System.out.println("print true");
+    }
 
 
 }
